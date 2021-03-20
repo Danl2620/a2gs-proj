35ca4528171770aa4c3df236851d408c1139e2ff
Dan Liebgold <dan.liebgold@gmail.com>
2021-03-20 15:47:43 -0700

    WIP improve assembly

--- assemble.sh
+++ assemble.sh
@@ -2,8 +2,12 @@
 ls src/*.S | xargs ./bin/cadius INDENTFILE
 ls src/*.S | xargs ./bin/merlin -V ./src/macro
 
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
 sed -ie 's/Type(00)/Type(B3)/' src/_FileInformation.txt
 
 ./bin/cadius DELETEFILE blankdisk.po NEW.DISK/main
 ./bin/cadius ADDFILE blankdisk.po NEW.DISK src/main
-gsplus
+/Applications/GSPlus.app/Contents/MacOS/gsplus
