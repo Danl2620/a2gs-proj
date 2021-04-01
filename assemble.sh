9c905fca2b7845246568311acdcc16f155228d64
Dan Liebgold <dan.liebgold@gmail.com>
2021-04-01 00:06:54 -0700

    assemble can work on different files

--- assemble.sh
+++ assemble.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 ##ls src/*.S | xargs ./bin/cadius INDENTFILE
-ls src/*.S | xargs ./bin/merlin -V ./src/macro
+
+##NAME=main
+NAME=shrhello
+
+./bin/merlin -V ./src/macro ./src/${NAME}.s
 
 if [ $? -ne 0 ]; then
     exit $?
@@ -8,6 +12,6 @@ fi
 
 sed -ie 's/Type(00)/Type(B3)/' src/_FileInformation.txt
 
-./bin/cadius DELETEFILE blankdisk.po NEW.DISK/main
-./bin/cadius ADDFILE blankdisk.po NEW.DISK src/main
+./bin/cadius DELETEFILE blankdisk.po NEW.DISK/${NAME}
+./bin/cadius ADDFILE blankdisk.po NEW.DISK src/${NAME}
 /Applications/GSPlus.app/Contents/MacOS/gsplus
