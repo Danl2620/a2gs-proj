0ad0a3a68120142754e7df343896325a6388a68f
Dan Liebgold <dliebgold@wonderstorm.net>
2023-01-20 19:30:12 -0800

    move projdisk into images/

--- assemble.sh
+++ assemble.sh
@@ -6,8 +6,7 @@ PRODOS_IMAGE=images/ProDOS_2_4_2.dsk
 NAME=main
 ##NAME=shrhello
 
-##DISKNAME=blankdisk
-DISKNAME=projdisk
+DISKNAME=images/projdisk.po
 VOLUMENAME=PROJ
 FOLDERNAME=PROJ
 
@@ -23,14 +22,14 @@ sed -ie 's/Type(00)/Type(B3)/' src/_FileInformation.txt
 ./ac.sh -g ${PRODOS_IMAGE} BASIC.SYSTEM > files/BASIC.SYSTEM
 
 ## create the boot disk
-rm ${DISKNAME}.po
-./bin/cadius CREATEVOLUME ${DISKNAME}.po ${VOLUMENAME} 800kb
-./bin/cadius CREATEFOLDER ${DISKNAME}.po ${FOLDERNAME}
-##./bin/cadius ADDFILE ${DISKNAME}.po ${FOLDERNAME} src/${NAME}
-./bin/cadius ADDFILE ${DISKNAME}.po ${FOLDERNAME} files/PRODOS
-./bin/cadius ADDFILE ${DISKNAME}.po ${FOLDERNAME} files/BASIC.SYSTEM
+rm ${DISKNAME}
+./bin/cadius CREATEVOLUME ${DISKNAME} ${VOLUMENAME} 800kb
+./bin/cadius CREATEFOLDER ${DISKNAME} ${FOLDERNAME}
+##./bin/cadius ADDFILE ${DISKNAME} ${FOLDERNAME} src/${NAME}
+./bin/cadius ADDFILE ${DISKNAME} ${FOLDERNAME} files/PRODOS
+./bin/cadius ADDFILE ${DISKNAME} ${FOLDERNAME} files/BASIC.SYSTEM
 
 ## add the built file to the disk
-./ac.sh -p ${DISKNAME}.po ${NAME} bin 0x2000 < src/${NAME}
+./ac.sh -p ${DISKNAME} ${NAME} bin 0x2000 < src/${NAME}
 
 /Applications/GSPlus.app/Contents/MacOS/gsplus
